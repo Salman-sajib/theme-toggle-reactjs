173b7a7acd7cc2eee29c15d44f53c89e65af28be
Salman-sajib <sfsajib@gmail.com>
2024-02-10 12:58:06 +0600

    created the theme context, Imported the themebtn and card components to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
+
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl text-orange-700'>Hello React</h1>
+    <div className='flex flex-wrap min-h-screen items-center'>
+      <div className='w-full'>
+        <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
+          <ThemeBtn />
+        </div>
+
+        <div className='w-full max-w-sm mx-auto'>
+          <Card />
+        </div>
+      </div>
     </div>
   );
 }
